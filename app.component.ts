9a1f63dc52d232a74b7f508fd506484b52fe7661
Chaitanya18698 <50976982+Chaitanya18698@users.noreply.github.com>
2019-06-18 17:11:20 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { log } from 'util';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  data: any = {};
+
+  onSubmit() {
+    alert(JSON.stringify(this.data));
+    
+  }
+}
